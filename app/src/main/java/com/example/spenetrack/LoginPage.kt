93e064e2d48ccfafbbd4debe6969a516150a82b4
siannun <siannun2003@gmail.com>
2024-10-28 18:13:33 +0700

    add more page

--- app/src/main/java/com/example/spenetrack/LoginPage.kt
+++ app/src/main/java/com/example/spenetrack/LoginPage.kt
@@ -3,25 +3,33 @@ package com.example.spenetrack
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val button = findViewById<Button>(R.id.btnLetStart);
+
+        val button = findViewById<Button>(R.id.btnLogin);
         button.setOnClickListener{
-            val intent = Intent(this,LoginPage::class.java);
+            val intent = Intent(this,ListView::class.java);
             startActivity(intent);
+        }
+
+        val link = findViewById<TextView>(R.id.linkSignup);
+        link.setOnClickListener{
+            val intent = Intent(this,Signup_page::class.java);
+            startActivity(intent)
         }
 
     }
--- app/src/main/java/com/example/spenetrack/Signup_page.kt
+++ app/src/main/java/com/example/spenetrack/Signup_page.kt
@@ -3,26 +3,32 @@ package com.example.spenetrack
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Signup_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val button = findViewById<Button>(R.id.btnLetStart);
+        val button = findViewById<Button>(R.id.btnSignup);
         button.setOnClickListener{
-            val intent = Intent(this,LoginPage::class.java);
+            val intent = Intent(this,ListView::class.java);
             startActivity(intent);
         }
 
+        val linkToLogin =  findViewById<TextView>(R.id.linkLogin);
+        linkToLogin.setOnClickListener{
+            val intent = Intent(this,LoginPage::class.java);
+            startActivity(intent);
+        }
     }
 }
